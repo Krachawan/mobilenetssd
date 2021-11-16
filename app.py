069361c208f5afb61a47239e2027a7c424bda683
Krachawan <89436904+Krachawan@users.noreply.github.com>
2021-11-16 14:39:56 +0700

    Update app.py

--- app.py
+++ app.py
@@ -150,7 +150,7 @@ def event_handle(event):
     if msgType == "text":
         msg = str(event["message"]["text"])
         if msg == "สวัสดี":
-            replyObj = TextSendMessage(text="欢迎山加Osvaldo")
+            replyObj = TextSendMessage(text="欢迎参加Osvaldo")
         elif:
         replyObj = TextSendMessage(text="ไม่รู้ไม่ชี้")
         line_bot_api.reply_message(rtoken, replyObj)
